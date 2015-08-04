9467989b770cbaba54efb570a6c67e7dd85b6489
Wizzercn <wizzer@qq.com>
2015-08-04 22:35:53 +0800

    菜单管理
    做一半啦

---
+++ src/main/java/cn/wizzer/modules/sys/MenuAction.java
@@ -0,0 +1,146 @@
+package cn.wizzer.modules.sys;
+
+import cn.wizzer.common.Message;
+import cn.wizzer.common.annotation.SLog;
+import cn.wizzer.common.mvc.filter.PrivateFilter;
+import cn.wizzer.modules.sys.bean.Sys_menu;
+import cn.wizzer.modules.sys.service.MenuService;
+import cn.wizzer.modules.sys.service.UnitService;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.nutz.dao.Cnd;
+import org.nutz.dao.Sqls;
+import org.nutz.dao.entity.Record;
+import org.nutz.ioc.loader.annotation.Inject;
+import org.nutz.ioc.loader.annotation.IocBean;
+import org.nutz.lang.Strings;
+import org.nutz.mvc.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+/**
+ * Created by Wizzer.cn on 2015/7/4.
+ */
+@IocBean
+@At("/private/sys/menu")
+@Filters({@By(type = PrivateFilter.class)})
+@SLog(tag = "菜单管理", msg = "")
+public class MenuAction {
+    @Inject
+    MenuService menuService;
+
+    @At("")
+    @Ok("vm:template.private.sys.menu.index")
+    @RequiresPermissions("sys:menu")
+    public Object index() {
+        return menuService.query(Cnd.where("parentId", "=", "").asc("location").asc("path"), null);
+    }
+
+    @At("/add")
+    @Ok("vm:template.private.sys.menu.add")
+    @RequiresPermissions("sys:menu")
+    public void add(@Param("pid") String pid, HttpServletRequest req) {
+        if (!Strings.isEmpty(pid)) {
+            req.setAttribute("parentMenu", menuService.fetch(pid));
+        }
+    }
+
+    @At("/add/do")
+    @Ok("json")
+    @RequiresPermissions("sys:menu")
+    @SLog(tag = "新建菜单", msg = "菜单名称：${args[0].name}")
+    public Object addDo(@Param("..") Sys_menu menu, @Param("parentId") String parentId, HttpServletRequest req) {
+        int sum = menuService.count(Cnd.where("parentId", "=", parentId).and("name", "=", menu.getName()));
+        if (sum > 0) {
+            return Message.error("菜单名称已存在！", req);
+        }
+        try {
+            menuService.save(menu, parentId);
+            return Message.success("system.success", req);
+        } catch (Exception e) {
+            return Message.error("system.error", req);
+        }
+    }
+
+    @At("/edit/?")
+    @Ok("vm:template.private.sys.menu.edit")
+    @RequiresPermissions("sys:menu")
+    public Object edit(String id, HttpServletRequest req) {
+        Sys_menu menu = menuService.fetch(id);
+        if (menu != null && !Strings.isEmpty(menu.getParentId())) {
+            req.setAttribute("parentMenu", menuService.fetch(menu.getParentId()));
+        }
+        return menu;
+    }
+
+    @At("/edit/do")
+    @Ok("json")
+    @RequiresPermissions("sys:menu")
+    @SLog(tag = "修改菜单", msg = "菜单名称：${args[0].name}")
+    public Object editDo(@Param("..") Sys_menu menu, @Param("pid") String pid, HttpServletRequest req) {
+        if (menu.getParentId().equals(menu.getId())) {
+            return Message.error("上级菜单不可为自身！", req);
+        }
+        if (!Strings.sNull(pid).equals(menu.getParentId())) {
+            int sum = menuService.count(Cnd.where("parentId", "=", menu.getParentId()).and("name", "=", menu.getName()));
+            if (sum > 0) {
+                return Message.error("菜单名称已存在！", req);
+            }
+        }
+        try {
+            menuService.edit(menu, pid);
+            return Message.success("system.success", req);
+        } catch (Exception e) {
+            return Message.success("system.error", req);
+        }
+    }
+
+    @At("/detail/?")
+    @Ok("vm:template.private.sys.menu.detail")
+    @RequiresPermissions("sys:menu")
+    public Object detail(String id, HttpServletRequest req) {
+        Sys_menu menu = menuService.fetch(id);
+        if (menu != null && !Strings.isEmpty(menu.getParentId())) {
+            req.setAttribute("parentMenu", menuService.getField("name", menu.getParentId()).getName());
+        }
+        return menu;
+    }
+
+    @At("/child/?")
+    @Ok("vm:template.private.sys.menu.child")
+    @RequiresPermissions("sys:menu")
+    public Object child(String id, HttpServletRequest req) {
+        return menuService.query(Cnd.where("parentId", "=", id), null);
+    }
+
+    @At("/tree")
+    @Ok("json")
+    @RequiresPermissions("sys:menu")
+    public Object tree(@Param("pid") String pid, HttpServletRequest req) {
+        List<Record> list;
+        if (!Strings.isEmpty(pid)) {
+            list = menuService.list(Sqls.create("select id,name as text,has_children as children from sys_menu where parentId =@pid order by location asc,path asc").setParam("pid", pid));
+        } else {
+            list = menuService.list(Sqls.create("select id,name as text,has_children as children from sys_menu where length(path)=4 order by location asc,path asc"));
+        }
+        return list;
+    }
+
+    @At("/delete/?")
+    @Ok("json")
+    @RequiresPermissions("sys:menu")
+    @SLog(tag = "删除菜单", msg = "菜单名称：${args[1].getAttribute('name')}")
+    public Object delete(String id, HttpServletRequest req) {
+        Sys_menu menu = menuService.fetch(id);
+        req.setAttribute("name", menu.getName());
+        if ("0001".equals(menu.getPath())) {
+            return Message.error("system.not.allow", req);
+        }
+        try {
+            menuService.deleteAndChild(id);
+            return Message.success("system.success", req);
+        } catch (Exception e) {
+            return Message.error("system.error", req);
+        }
+    }
+}
--- src/main/java/cn/wizzer/modules/sys/service/MenuService.java
+++ src/main/java/cn/wizzer/modules/sys/service/MenuService.java
@@ -1,17 +1,116 @@
 package cn.wizzer.modules.sys.service;
 
 import cn.wizzer.common.service.core.BaseService;
+import cn.wizzer.common.util.StringUtils;
 import cn.wizzer.modules.sys.bean.Sys_menu;
+import org.nutz.aop.interceptor.ioc.TransAop;
+import org.nutz.dao.Chain;
+import org.nutz.dao.Cnd;
 import org.nutz.dao.Dao;
+import org.nutz.dao.Sqls;
+import org.nutz.ioc.aop.Aop;
 import org.nutz.ioc.loader.annotation.IocBean;
+import org.nutz.lang.Strings;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by Wizzer.cn on 2015/7/25.
  */
 @IocBean(args = {"refer:dao"})
-public class MenuService  extends BaseService<Sys_menu> {
+public class MenuService extends BaseService<Sys_menu> {
     public MenuService(Dao dao) {
         super(dao);
     }
 
+    /**
+     * 级联删除菜单
+     *
+     * @param id
+     */
+    @Aop(TransAop.READ_COMMITTED)
+    public void deleteAndChild(String id) {
+        String pid = this.fetch(id).getParentId();
+        dao().execute(Sqls.create("delete from sys_menu where id = @id").setParam("id", id));
+        dao().execute(Sqls.create("delete from sys_menu where parentId = @id").setParam("id", id));
+        dao().execute(Sqls.create("delete from sys_role_menu where menu_id=@id or menu_id in(SELECT id FROM sys_menu WHERE parentId=@id)").setParam("id", id));
+        if (!Strings.isEmpty(pid)) {
+            int count = count(Cnd.where("parentId", "=", pid));
+            if (count < 1) {
+                dao().execute(Sqls.create("update sys_menu set has_children=false where id=@pid").setParam("pid", pid));
+            }
+        }
+    }
+
+    /**
+     * 新增菜单
+     *
+     * @param menu
+     * @param pid
+     */
+    @Aop(TransAop.READ_COMMITTED)
+    public void save(Sys_menu menu, String pid) {
+        String path = "";
+        if (!Strings.isEmpty(pid)) {
+            Sys_menu pp = this.fetch(pid);
+            path = pp.getPath();
+        }
+        menu.setPath(getSubPath("sys_menu", "path", path));
+        menu.setParentId(pid);
+        dao().insert(menu);
+        if (!Strings.isEmpty(pid)) {
+            dao().execute(Sqls.create("update sys_menu set has_children=true where id=@pid").setParam("pid", pid));
+        }
+    }
+
+    /**
+     * 修改菜单
+     *
+     * @param menu
+     * @param pid
+     */
+    @Aop(TransAop.READ_COMMITTED)
+    public void edit(Sys_menu menu, String pid) {
+        if (!Strings.sNull(pid).equals(menu.getParentId())) {
+            if (!Strings.isEmpty(menu.getParentId())) {
+                Sys_menu pp = this.fetch(menu.getParentId());
+                menu.setPath(getSubPath("sys_menu", "path", pp.getPath()));
+                menu.setParentId(pp.getId());
+            } else {
+                menu.setPath(getSubPath("sys_menu", "path", ""));
+                menu.setParentId("");
+            }
+        }
+        menu.setCreateTime(new Date());
+        menu.setCreateUser(StringUtils.getUid());
+        dao().update(menu);
+        if (!Strings.isEmpty(pid)) {
+            int count = count(Cnd.where("parentId", "=", pid));
+            if (count < 1) {
+                dao().execute(Sqls.create("update sys_menu set has_children=false where id=@pid").setParam("pid", pid));
+            }
+        }
+        if (!Strings.isEmpty(menu.getParentId()))
+            dao().execute(Sqls.create("update sys_menu set has_children=true where id=@id").setParam("id", menu.getParentId()));
+        if (menu.isHasChildren()) {
+            updatePath(menu.getId(), menu.getPath());
+        }
+    }
+
+    /**
+     * 更新子节点path
+     *
+     * @param id
+     * @param path
+     */
+    public void updatePath(String id, String path) {
+        List<Sys_menu> list = this.query(Cnd.where("parentId", "=", id), null);
+        for (Sys_menu menu : list) {
+            this.update(Chain.make("path", getSubPath("sys_menu", "path", path)), Cnd.where("id", "=", menu.getId()));
+            if (menu.isHasChildren()) {
+                updatePath(menu.getId(), menu.getPath());
+            }
+        }
+    }
 }
