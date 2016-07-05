008ae149c2d35dac6b5be4363883e9f0cb3794e0
Wizzer <wizzer@qq.com>
2016-07-05 22:31:56 +0800

    add:微信回复图文

--- src/main/java/cn/wizzer/modules/back/wx/controllers/WxReplyNewsController.java
+++ src/main/java/cn/wizzer/modules/back/wx/controllers/WxReplyNewsController.java
@@ -1,13 +1,13 @@
 package cn.wizzer.modules.back.wx.controllers;
 
 import cn.wizzer.common.annotation.SLog;
-import cn.wizzer.common.base.Globals;
 import cn.wizzer.common.base.Result;
 import cn.wizzer.common.filter.PrivateFilter;
 import cn.wizzer.common.page.DataTableColumn;
 import cn.wizzer.common.page.DataTableOrder;
-import cn.wizzer.modules.back.sys.models.Sys_config;
+import cn.wizzer.modules.back.wx.models.Wx_reply_news;
 import cn.wizzer.modules.back.wx.models.Wx_reply_txt;
+import cn.wizzer.modules.back.wx.services.WxReplyNewsService;
 import cn.wizzer.modules.back.wx.services.WxReplyTxtService;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
@@ -15,7 +15,6 @@ import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.nutz.dao.Cnd;
 import org.nutz.ioc.loader.annotation.Inject;
 import org.nutz.ioc.loader.annotation.IocBean;
-import org.nutz.lang.Strings;
 import org.nutz.log.Log;
 import org.nutz.log.Logs;
 import org.nutz.mvc.annotation.*;
@@ -27,22 +26,22 @@ import java.util.List;
  * Created by Wizzer on 2016/7/5.
  */
 @IocBean
-@At("/private/wx/reply/txt")
+@At("/private/wx/reply/news")
 @Filters({@By(type = PrivateFilter.class)})
-public class WxReplyTxtController {
+public class WxReplyNewsController {
     private static final Log log = Logs.get();
     @Inject
-    WxReplyTxtService wxReplyTxtService;
+    WxReplyNewsService wxReplyNewsService;
 
     @At("")
-    @Ok("beetl:/private/wx/reply/txt/index.html")
+    @Ok("beetl:/private/wx/reply/news/index.html")
     @RequiresAuthentication
     public void index() {
 
     }
 
     @At
-    @Ok("beetl:/private/wx/reply/txt/add.html")
+    @Ok("beetl:/private/wx/reply/news/add.html")
     @RequiresAuthentication
     public void add() {
 
@@ -50,11 +49,11 @@ public class WxReplyTxtController {
 
     @At
     @Ok("json")
-    @RequiresPermissions("wx.reply.txt.add")
+    @RequiresPermissions("wx.reply.news.add")
     @SLog(tag = "添加回复文本", msg = "文本标题:${args[0].title}")
-    public Object addDo(@Param("..") Wx_reply_txt txt, HttpServletRequest req) {
+    public Object addDo(@Param("..") Wx_reply_news news, HttpServletRequest req) {
         try {
-            wxReplyTxtService.insert(txt);
+            wxReplyNewsService.insert(news);
             return Result.success("system.success", req);
         } catch (Exception e) {
             return Result.error("system.error", req);
@@ -62,19 +61,19 @@ public class WxReplyTxtController {
     }
 
     @At("/edit/?")
-    @Ok("beetl:/private/wx/reply/txt/edit.html")
+    @Ok("beetl:/private/wx/reply/news/edit.html")
     @RequiresAuthentication
     public Object edit(String id) {
-        return wxReplyTxtService.fetch(id);
+        return wxReplyNewsService.fetch(id);
     }
 
     @At
     @Ok("json")
-    @RequiresPermissions("wx.reply.txt.edit")
+    @RequiresPermissions("wx.reply.news.edit")
     @SLog(tag = "修改回复文本", msg = "文本标题:${args[0].title}")
-    public Object editDo(@Param("..") Wx_reply_txt txt, HttpServletRequest req) {
+    public Object editDo(@Param("..") Wx_reply_news news, HttpServletRequest req) {
         try {
-            wxReplyTxtService.updateIgnoreNull(txt);
+            wxReplyNewsService.updateIgnoreNull(news);
             return Result.success("system.success", req);
         } catch (Exception e) {
             return Result.error("system.error", req);
@@ -83,12 +82,12 @@ public class WxReplyTxtController {
 
     @At("/delete/?")
     @Ok("json")
-    @RequiresPermissions("wx.reply.txt.delete")
+    @RequiresPermissions("wx.reply.news.delete")
     @SLog(tag = "删除回复文本", msg = "文本标题:${args[1].getAttribute('title')}}")
     public Object delete(String id, HttpServletRequest req) {
         try {
-            req.setAttribute("title", wxReplyTxtService.fetch(id).getTitle());
-            wxReplyTxtService.delete(id);
+            req.setAttribute("title", wxReplyNewsService.fetch(id).getTitle());
+            wxReplyNewsService.delete(id);
             return Result.success("system.success", req);
         } catch (Exception e) {
             return Result.error("system.error", req);
@@ -97,11 +96,11 @@ public class WxReplyTxtController {
 
     @At("/delete")
     @Ok("json")
-    @RequiresPermissions("wx.reply.txt.delete")
+    @RequiresPermissions("wx.reply.news.delete")
     @SLog(tag = "删除回复文本", msg = "ID:${args[0]}")
     public Object deletes(@Param("ids") String id, HttpServletRequest req) {
         try {
-            wxReplyTxtService.delete(StringUtils.split(id, ","));
+            wxReplyNewsService.delete(StringUtils.split(id, ","));
             return Result.success("system.success", req);
         } catch (Exception e) {
             return Result.error("system.error", req);
@@ -113,6 +112,6 @@ public class WxReplyTxtController {
     @RequiresAuthentication
     public Object data(@Param("length") int length, @Param("start") int start, @Param("draw") int draw, @Param("::order") List<DataTableOrder> order, @Param("::columns") List<DataTableColumn> columns) {
         Cnd cnd = Cnd.NEW();
-        return wxReplyTxtService.data(length, start, draw, order, columns, cnd, null);
+        return wxReplyNewsService.data(length, start, draw, order, columns, cnd, null);
     }
 }
