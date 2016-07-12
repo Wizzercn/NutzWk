e11c32945e788bdf2623e42326fde32339d2012f
Wizzer <wizzer@qq.com>
2016-07-12 10:13:58 +0800

    fixed: #22

--- src/main/java/cn/wizzer/common/util/StringUtil.java
+++ src/main/java/cn/wizzer/common/util/StringUtil.java
@@ -7,12 +7,42 @@ import org.nutz.mvc.Mvcs;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Random;
+import java.util.regex.Pattern;
 
 /**
  * Created by Wizzer.cn on 2015/7/4.
  */
 @IocBean
 public class StringUtil {
+
+    private static final Pattern IPV4_PATTERN =
+            Pattern.compile(
+                    "^(25[0-5]|2[0-4]\\d|[0-1]?\\d?\\d)(\\.(25[0-5]|2[0-4]\\d|[0-1]?\\d?\\d)){3}$");
+
+    private static final Pattern IPV6_STD_PATTERN =
+            Pattern.compile(
+                    "^(?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$");
+
+    private static final Pattern IPV6_HEX_COMPRESSED_PATTERN =
+            Pattern.compile(
+                    "^((?:[0-9A-Fa-f]{1,4}(?::[0-9A-Fa-f]{1,4})*)?)::((?:[0-9A-Fa-f]{1,4}(?::[0-9A-Fa-f]{1,4})*)?)$");
+
+    public static boolean isIPv4Address(final String input) {
+        return IPV4_PATTERN.matcher(input).matches();
+    }
+
+    public static boolean isIPv6StdAddress(final String input) {
+        return IPV6_STD_PATTERN.matcher(input).matches();
+    }
+
+    public static boolean isIPv6HexCompressedAddress(final String input) {
+        return IPV6_HEX_COMPRESSED_PATTERN.matcher(input).matches();
+    }
+
+    public static boolean isIPv6Address(final String input) {
+        return isIPv6StdAddress(input) || isIPv6HexCompressedAddress(input);
+    }
+
     /**
      * 计算MD5密码
      *
@@ -39,7 +69,11 @@ public class StringUtil {
         } else if (Strings.isNotBlank(remoteAddr)) {
             remoteAddr = request.getHeader("WL-Proxy-Client-IP");
         }
-        return remoteAddr != null ? remoteAddr : request.getRemoteAddr();
+        String ip = remoteAddr != null ? remoteAddr : request.getRemoteAddr();
+        if (isIPv4Address(remoteAddr) || isIPv6Address(remoteAddr)) {
+            return ip;
+        }
+        return "";
     }
 
     /**
@@ -85,6 +119,7 @@ public class StringUtil {
 
     /**
      * 判断是否以字符串开头
+     *
      * @param str
      * @param s
      * @return
@@ -92,4 +127,5 @@ public class StringUtil {
     public boolean startWith(String str, String s) {
         return Strings.sNull(str).startsWith(Strings.sNull(s));
     }
+
 }
