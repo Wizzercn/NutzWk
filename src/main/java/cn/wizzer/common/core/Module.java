c222c855b938b2123e769c5f2689bfbd51a69062
Wizzer <wizzer@qq.com>
2016-07-15 09:58:01 +0800

    add: @Async注解支持,实现方法异步执行

--- src/main/java/cn/wizzer/common/core/Module.java
+++ src/main/java/cn/wizzer/common/core/Module.java
@@ -8,15 +8,15 @@ import org.nutz.mvc.ioc.provider.ComboIocProvider;
 /**
  * Created by wizzer on 2016/6/21.
  */
-@Modules(scanPackage = true,packages = "cn.wizzer.modules")
+@Modules(scanPackage = true, packages = "cn.wizzer.modules")
 @Ok("json:full")
 @Fail("http:500")
-@IocBy(type=ComboIocProvider.class,args={"*json","config/ioc/","*anno","cn.wizzer","*tx","*org.nutz.integration.quartz.QuartzIocLoader"})
-@Localization(value="locales/", defaultLocalizationKey="zh_CN")
+@IocBy(type = ComboIocProvider.class, args = {"*json", "config/ioc/", "*anno", "cn.wizzer", "*tx", "*quartz", "*async"})
+@Localization(value = "locales/", defaultLocalizationKey = "zh_CN")
 @Encoding(input = "UTF-8", output = "UTF-8")
-@Views({ BeetlViewMaker.class})
-@SetupBy(value=Setup.class)
-@ChainBy(args="config/chain/nutzwk-mvc-chain.json")
+@Views({BeetlViewMaker.class})
+@SetupBy(value = Setup.class)
+@ChainBy(args = "config/chain/nutzwk-mvc-chain.json")
 @SessionBy(ShiroSessionProvider.class)
 public class Module {
 }
