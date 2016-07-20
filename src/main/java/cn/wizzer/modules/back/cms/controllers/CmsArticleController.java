f0a2de27b7f1e38a9abd1e0998e9433f0ea43459
Wizzer <wizzer@qq.com>
2016-07-20 22:40:07 +0800

    add: CMS文章

--- src/main/java/cn/wizzer/modules/back/cms/controllers/CmsArticleController.java
+++ src/main/java/cn/wizzer/modules/back/cms/controllers/CmsArticleController.java
@@ -5,11 +5,13 @@ import cn.wizzer.common.base.Result;
 import cn.wizzer.common.filter.PrivateFilter;
 import cn.wizzer.common.page.DataTableColumn;
 import cn.wizzer.common.page.DataTableOrder;
+import cn.wizzer.common.util.DateUtil;
+import cn.wizzer.modules.back.cms.models.Cms_article;
 import cn.wizzer.modules.back.cms.models.Cms_channel;
-import cn.wizzer.modules.back.cms.models.Cms_site;
 import cn.wizzer.modules.back.cms.services.CmsArticleService;
 import cn.wizzer.modules.back.cms.services.CmsChannelService;
 import cn.wizzer.modules.back.sys.models.Sys_user;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
@@ -24,10 +26,8 @@ import org.nutz.mvc.adaptor.WhaleAdaptor;
 import org.nutz.mvc.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * Created by wizzer on 2016/6/28.
@@ -88,10 +88,102 @@ public class CmsArticleController {
     @At
     @Ok("beetl:/private/cms/article/add.html")
     @RequiresAuthentication
-    public void add(@Param("channelId") String channelId,HttpServletRequest req){
-        req.setAttribute("channel",channelId!=null&&!"0".equals(channelId)?cmsChannelService.fetch(channelId):null);
+    public void add(@Param("channelId") String channelId, HttpServletRequest req) {
+        req.setAttribute("channel", channelId != null && !"0".equals(channelId) ? cmsChannelService.fetch(channelId) : null);
         Subject subject = SecurityUtils.getSubject();
         Sys_user user = (Sys_user) subject.getPrincipal();
-        req.setAttribute("nickname",user==null?"":user.getNickname());
+        req.setAttribute("nickname", user == null ? "" : user.getNickname());
+    }
+
+    @At
+    @Ok("json")
+    @RequiresPermissions("cms.content.article.add")
+    @SLog(tag = "添加文章", msg = "文章标题:${args[0].title}")
+    @AdaptBy(type = WhaleAdaptor.class)
+    public Object addDo(@Param("..") Cms_article article, @Param("at") String at, HttpServletRequest req) {
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            int publishAt = (int) (sdf.parse(at).getTime() / 1000);
+            article.setPublishAt(publishAt);
+            cmsArticleService.insert(article);
+            return Result.success("system.success");
+        } catch (Exception e) {
+            return Result.error("system.error");
+        }
+    }
+
+    @At("/edit/?")
+    @Ok("beetl:/private/cms/article/edit.html")
+    @RequiresAuthentication
+    public Object edit(String id, HttpServletRequest req) {
+        Cms_article article = cmsArticleService.fetch(id);
+        req.setAttribute("channel", article != null ? cmsChannelService.fetch(article.getChannelId()) : null);
+        return article;
+    }
+
+    @At
+    @Ok("json")
+    @RequiresPermissions("cms.content.article.edit")
+    @SLog(tag = "添加文章", msg = "文章标题:${args[0].title}")
+    @AdaptBy(type = WhaleAdaptor.class)
+    public Object editDo(@Param("..") Cms_article article, @Param("at") String at, HttpServletRequest req) {
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            int publishAt = (int) (sdf.parse(at).getTime() / 1000);
+            article.setPublishAt(publishAt);
+            cmsArticleService.updateIgnoreNull(article);
+            return Result.success("system.success");
+        } catch (Exception e) {
+            return Result.error("system.error");
+        }
+    }
+
+
+    @At("/enable/?")
+    @Ok("json")
+    @RequiresPermissions("cms.content.article.edit")
+    @SLog(tag = "发布文章", msg = "文章标题:${args[1].getAttribute('title')}")
+    public Object enable(String id, HttpServletRequest req) {
+        try {
+            req.setAttribute("title", cmsArticleService.fetch(id).getTitle());
+            cmsArticleService.update(org.nutz.dao.Chain.make("disabled", false), Cnd.where("id", "=", id));
+            return Result.success("system.success");
+        } catch (Exception e) {
+            return Result.error("system.error");
+        }
+    }
+
+    @At("/disable/?")
+    @Ok("json")
+    @RequiresPermissions("cms.content.article.edit")
+    @SLog(tag = "取消发布", msg = "文章标题:${args[1].getAttribute('title')}")
+    public Object disable(String id, HttpServletRequest req) {
+        try {
+            req.setAttribute("title", cmsArticleService.fetch(id).getTitle());
+            cmsArticleService.update(org.nutz.dao.Chain.make("disabled", true), Cnd.where("id", "=", id));
+            return Result.success("system.success");
+        } catch (Exception e) {
+            return Result.error("system.error");
+        }
+    }
+
+    @At({"/delete/?", "/delete"})
+    @Ok("json")
+    @RequiresPermissions("cms.content.article.delete")
+    @SLog(tag = "删除文章", msg = "ID:${args[2].getAttribute('id')}")
+    public Object delete(String oneId, @Param("ids") String ids, HttpServletRequest req) {
+        try {
+            if (ids != null) {
+                String[] str = StringUtils.split(ids, ",");
+                cmsArticleService.delete(str);
+                req.setAttribute("id", ids);
+            } else {
+                cmsArticleService.delete(oneId);
+                req.setAttribute("id", oneId);
+            }
+            return Result.success("system.success");
+        } catch (Exception e) {
+            return Result.error("system.error");
+        }
     }
 }
