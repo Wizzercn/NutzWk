996339af37a86c1b4268bc615130b5fac6fe7279
Wizzer <wizzer@qq.com>
2016-08-12 15:48:33 +0800

    add: 数据字典管理

---
+++ src/main/java/cn/wizzer/modules/back/sys/controllers/DictController.java
@@ -0,0 +1,125 @@
+package cn.wizzer.modules.back.sys.controllers;
+
+import cn.wizzer.common.annotation.SLog;
+import cn.wizzer.common.base.Result;
+import cn.wizzer.common.filter.PrivateFilter;
+import cn.wizzer.modules.back.sys.models.Sys_dict;
+import cn.wizzer.modules.back.sys.services.DictService;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.nutz.dao.Cnd;
+import org.nutz.ioc.loader.annotation.Inject;
+import org.nutz.ioc.loader.annotation.IocBean;
+import org.nutz.lang.Strings;
+import org.nutz.log.Log;
+import org.nutz.log.Logs;
+import org.nutz.mvc.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by wizzer on 2016/6/24.
+ */
+@IocBean
+@At("/private/sys/dict")
+@Filters({@By(type = PrivateFilter.class)})
+public class DictController {
+    private static final Log log = Logs.get();
+    @Inject
+    DictService dictService;
+
+    @At("")
+    @Ok("beetl:/private/sys/dict/index.html")
+    @RequiresAuthentication
+    public Object index() {
+        return dictService.query(Cnd.where("parentId", "=", "").asc("path"));
+    }
+
+    @At
+    @Ok("beetl:/private/sys/dict/add.html")
+    @RequiresAuthentication
+    public Object add(@Param("pid") String pid) {
+        return Strings.isBlank(pid) ? null : dictService.fetch(pid);
+    }
+
+    @At
+    @Ok("json")
+    @RequiresPermissions("sys.manager.dict.add")
+    @SLog(tag = "新建字典", msg = "字典名称:${args[0].name}")
+    public Object addDo(@Param("..") Sys_dict dict, @Param("parentId") String parentId, HttpServletRequest req) {
+        try {
+            dictService.save(dict, parentId);
+            return Result.success("system.success");
+        } catch (Exception e) {
+            return Result.error("system.error");
+        }
+    }
+
+    @At("/child/?")
+    @Ok("beetl:/private/sys/dict/child.html")
+    @RequiresAuthentication
+    public Object child(String id) {
+        return dictService.query(Cnd.where("parentId", "=", id).asc("path"));
+    }
+
+    @At("/edit/?")
+    @Ok("beetl:/private/sys/dict/edit.html")
+    @RequiresAuthentication
+    public Object edit(String id, HttpServletRequest req) {
+        Sys_dict dict = dictService.fetch(id);
+        if (dict != null) {
+            req.setAttribute("parentUnit", dictService.fetch(dict.getParentId()));
+        }
+        return dict;
+    }
+
+    @At
+    @Ok("json")
+    @RequiresPermissions("sys.manager.dict.edit")
+    @SLog(tag = "编辑字典", msg = "字典名称:${args[0].name}")
+    public Object editDo(@Param("..") Sys_dict dict, @Param("parentId") String parentId, HttpServletRequest req) {
+        try {
+            dict.setOpBy(Strings.sNull(req.getAttribute("uid")));
+            dict.setOpAt((int) (System.currentTimeMillis() / 1000));
+            dictService.updateIgnoreNull(dict);
+            return Result.success("system.success");
+        } catch (Exception e) {
+            return Result.error("system.error");
+        }
+    }
+
+    @At("/delete/?")
+    @Ok("json")
+    @RequiresPermissions("sys.manager.dict.delete")
+    @SLog(tag = "删除字典", msg = "字典名称:${args[1].getAttribute('name')}")
+    public Object delete(String id, HttpServletRequest req) {
+        try {
+            Sys_dict dict = dictService.fetch(id);
+            req.setAttribute("name", dict.getName());
+            dictService.deleteAndChild(dict);
+            return Result.success("system.success");
+        } catch (Exception e) {
+            return Result.error("system.error");
+        }
+    }
+
+    @At
+    @Ok("json")
+    @RequiresAuthentication
+    public Object tree(@Param("pid") String pid) {
+        List<Sys_dict> list = dictService.query(Cnd.where("parentId", "=", Strings.sBlank(pid)).asc("path"));
+        List<Map<String, Object>> tree = new ArrayList<>();
+        for (Sys_dict dict : list) {
+            Map<String, Object> obj = new HashMap<>();
+            obj.put("id", dict.getId());
+            obj.put("text", dict.getName());
+            obj.put("children", dict.isHasChildren());
+            tree.add(obj);
+        }
+        return tree;
+    }
+}
---
+++ src/main/java/cn/wizzer/modules/back/sys/models/Sys_dict.java
@@ -0,0 +1,107 @@
+package cn.wizzer.modules.back.sys.models;
+
+import cn.wizzer.common.base.Model;
+import org.nutz.dao.entity.annotation.*;
+
+import java.io.Serializable;
+
+/**
+ * Created by wizzer on 2016/6/21.
+ */
+@Table("sys_dict")
+@TableIndexes({@Index(name = "INDEX_SYS_DICT_PATH", fields = {"path"}, unique = true)})
+public class Sys_dict extends Model implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Column
+    @Name
+    @Comment("ID")
+    @ColDefine(type = ColType.VARCHAR, width = 32)
+    @Prev(els = {@EL("uuid()")})
+    private String id;
+
+    @Column
+    @Comment("父级ID")
+    @ColDefine(type = ColType.VARCHAR, width = 32)
+    private String parentId;
+
+    @Column
+    @Comment("树路径")
+    @ColDefine(type = ColType.VARCHAR, width = 100)
+    private String path;
+
+    @Column
+    @Comment("名称")
+    @ColDefine(type = ColType.VARCHAR, width = 100)
+    private String name;
+
+    @Column
+    @Comment("机构编码")
+    @ColDefine(type = ColType.VARCHAR, width = 20)
+    private String code;
+
+    @Column
+    @Comment("排序字段")
+    @Prev(@SQL("SELECT IFNULL(MAX(location),0)+1 FROM sys_dict"))
+    private Integer location;
+
+    @Column
+    @Comment("有子节点")
+    private boolean hasChildren;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(String parentId) {
+        this.parentId = parentId;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Integer getLocation() {
+        return location;
+    }
+
+    public void setLocation(Integer location) {
+        this.location = location;
+    }
+
+    public boolean isHasChildren() {
+        return hasChildren;
+    }
+
+    public void setHasChildren(boolean hasChildren) {
+        this.hasChildren = hasChildren;
+    }
+}
