5a874b7c05a95142ffd84ce03577f2c16f4c4355
Wizzer <wizzer@qq.com>
2016-09-02 23:33:30 +0800

    好吧 node版本问题

--- src/vue/config/index.js
+++ src/vue/config/index.js
@@ -4,10 +4,10 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../dist/index.html'),
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    index: path.resolve(__dirname, '../../main/webapp/WEB-INF/views/private/index.html'),
+    assetsRoot: path.resolve(__dirname, '../../main/webapp'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '{base}/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
