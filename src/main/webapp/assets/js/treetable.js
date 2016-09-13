f02bfd5246e2fe081811895a6e643b6aad0d9b75
Wizzer <wizzer@qq.com>
2016-09-13 18:49:30 +0800

    change: asstes文件夹可以清空了..

--- src/main/webapp/assets/js/treetable.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function () {
-    $("head").append("<link rel=\"stylesheet\" href=\""+base+"/include/plugins/treetable/jquery.treetable.css\">");
-    $("body").append("<script type=\"text/javascript\" src=\""+base+"/include/plugins/treetable/jquery.treetable.js\"></script>");
-});
