9d2902ca89c0671bf05ff20c6efff682746be324
Wizzercn <wizzer@qq.com>
2016-10-11 18:34:39 +0800

    update: .key

--- src/main/java/cn/wizzer/modules/back/sys/services/ApiService.java
+++ src/main/java/cn/wizzer/modules/back/sys/services/ApiService.java
@@ -37,7 +37,7 @@ public class ApiService extends Service<Sys_api> {
      * @return
      */
     public String generateToken(Date date, String appId) throws IOException, ClassNotFoundException {
-        File f = new File(Globals.AppRoot + "/WEB-INF/apikey/" + appId + ".txt");
+        File f = new File(Globals.AppRoot + "/WEB-INF/apikey/" + appId + ".key");
         if (Files.isFile(f)) {
             ObjectInputStream keyIn = new ObjectInputStream(new FileInputStream(f));
             key = (Key) keyIn.readObject();
@@ -67,7 +67,7 @@ public class ApiService extends Service<Sys_api> {
     public boolean verifyToken(String appId, String token) {
         try {
             if (key == null) {
-                ObjectInputStream keyIn = new ObjectInputStream(new FileInputStream(Globals.AppRoot + "/WEB-INF/apikey/" + appId + ".txt"));
+                ObjectInputStream keyIn = new ObjectInputStream(new FileInputStream(Globals.AppRoot + "/WEB-INF/apikey/" + appId + ".key"));
                 key = (Key) keyIn.readObject();
                 keyIn.close();
             }
