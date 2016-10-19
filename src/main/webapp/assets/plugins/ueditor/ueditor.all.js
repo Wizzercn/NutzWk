8dcbc8b7ac489e43fbd7df06b346195982fda340
Wizzercn <wizzer@qq.com>
2016-10-19 17:00:54 +0800

    fixed: #23 百度编辑器在模态框打开后不能全屏编辑的问题

--- src/main/webapp/assets/plugins/ueditor/ueditor.all.js
+++ src/main/webapp/assets/plugins/ueditor/ueditor.all.js
@@ -28926,9 +28926,23 @@ UE.ui = baidu.editor.ui = {};
                 }
                 if (fullscreen) {
                     while (container.tagName != "BODY") {
+                        var isModal = false;
+                        //判断该dom是否为modal
+                        var classes = $(container).attr('class');
+                        if (classes !== undefined) {
+                            classes = classes.split(' ');
+                            for (var i = 0; i < classes.length; i++) {
+                                if (classes[i] == "modal") {
+                                    isModal = true;
+                                }
+                            }
+                        }
                         var position = baidu.editor.dom.domUtils.getComputedStyle(container, "position");
                         nodeStack.push(position);
-                        container.style.position = "static";
+                        //如果是modal,则不设置position为static
+                        if (!isModal) {
+                            container.style.position = "static";
+                        }
                         container = container.parentNode;
                     }
                     this._bakHtmlOverflow = document.documentElement.style.overflow;
