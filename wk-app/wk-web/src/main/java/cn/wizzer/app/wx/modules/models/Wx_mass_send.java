efd9e65552a9a6d0cb8c2f6e89739a1b8c07f89f
Wizzer <wizzer@qq.com>
2017-02-13 12:10:13 +0800

    add: IDEA 插件

---
+++ wk-app/wk-web/src/main/java/cn/wizzer/app/wx/modules/models/Wx_mass_send.java
@@ -0,0 +1,130 @@
+package cn.wizzer.app.wx.modules.models;
+
+import cn.wizzer.framework.base.model.BaseModel;
+import org.nutz.dao.entity.annotation.*;
+
+import java.io.Serializable;
+
+/**
+ * Created by wizzer on 2016/7/2.
+ */
+@Table("wx_mass_send")
+public class Wx_mass_send extends BaseModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Column
+    @Name
+    @Comment("ID")
+    @ColDefine(type = ColType.VARCHAR, width = 32)
+    @Prev(els = {@EL("uuid()")})
+    private String id;
+
+    @Column
+    @Comment("群发ID")
+    @ColDefine(type = ColType.VARCHAR, width = 32)
+    private String massId;
+
+    @Column
+    @Comment("Openid列表")
+    @ColDefine(type = ColType.TEXT)
+    private String receivers;
+
+    @Column
+    @Comment("发送状态")
+    @ColDefine(type = ColType.INT)
+    protected Integer status;
+
+    @Column
+    @Comment("msgId")
+    @ColDefine(type = ColType.VARCHAR, width = 32)
+    private String msgId;
+
+    @Column
+    @Comment("errCode")
+    @ColDefine(type = ColType.VARCHAR, width = 32)
+    private String errCode;
+
+    @Column
+    @Comment("errMsg")
+    @ColDefine(type = ColType.VARCHAR, width = 255)
+    private String errMsg;
+
+    @Column
+    @Comment("微信ID")
+    @ColDefine(type = ColType.VARCHAR, width = 32)
+    private String wxid;
+
+    @One(field = "wxid")
+    private Wx_config wxConfig;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMassId() {
+        return massId;
+    }
+
+    public void setMassId(String massId) {
+        this.massId = massId;
+    }
+
+    public String getReceivers() {
+        return receivers;
+    }
+
+    public void setReceivers(String receivers) {
+        this.receivers = receivers;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getMsgId() {
+        return msgId;
+    }
+
+    public void setMsgId(String msgId) {
+        this.msgId = msgId;
+    }
+
+    public String getErrCode() {
+        return errCode;
+    }
+
+    public void setErrCode(String errCode) {
+        this.errCode = errCode;
+    }
+
+    public String getErrMsg() {
+        return errMsg;
+    }
+
+    public void setErrMsg(String errMsg) {
+        this.errMsg = errMsg;
+    }
+
+    public String getWxid() {
+        return wxid;
+    }
+
+    public void setWxid(String wxid) {
+        this.wxid = wxid;
+    }
+
+    public Wx_config getWxConfig() {
+        return wxConfig;
+    }
+
+    public void setWxConfig(Wx_config wxConfig) {
+        this.wxConfig = wxConfig;
+    }
+}
---
+++ wk-app/wk-web/src/main/java/cn/wizzer/app/wx/modules/models/Wx_reply.java
@@ -0,0 +1,104 @@
+package cn.wizzer.app.wx.modules.models;
+
+import cn.wizzer.framework.base.model.BaseModel;
+import org.nutz.dao.entity.annotation.*;
+
+import java.io.Serializable;
+
+/**
+ * Created by wizzer on 2016/7/2.
+ */
+@Table("wx_reply")
+public class Wx_reply extends BaseModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Column
+    @Name
+    @Comment("ID")
+    @ColDefine(type = ColType.VARCHAR, width = 32)
+    @Prev(els = {@EL("uuid()")})
+    private String id;
+
+    @Column
+    @Comment("回复类型")
+    @ColDefine(type = ColType.VARCHAR, width = 20)
+    private String type;
+
+    @Column
+    @Comment("消息类型")
+    @ColDefine(type = ColType.VARCHAR, width = 20)
+    private String msgType;
+
+    @Column
+    @Comment("关键词")
+    @ColDefine(type = ColType.VARCHAR, width = 50)
+    private String keyword;
+
+    @Column
+    @Comment("回复内容")
+    @ColDefine(type = ColType.TEXT)
+    private String content;
+
+    @Column
+    @Comment("微信ID")
+    @ColDefine(type = ColType.VARCHAR, width = 32)
+    private String wxid;
+
+    @One(field = "wxid")
+    private Wx_config wxConfig;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getMsgType() {
+        return msgType;
+    }
+
+    public void setMsgType(String msgType) {
+        this.msgType = msgType;
+    }
+
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getWxid() {
+        return wxid;
+    }
+
+    public void setWxid(String wxid) {
+        this.wxid = wxid;
+    }
+
+    public Wx_config getWxConfig() {
+        return wxConfig;
+    }
+
+    public void setWxConfig(Wx_config wxConfig) {
+        this.wxConfig = wxConfig;
+    }
+}
