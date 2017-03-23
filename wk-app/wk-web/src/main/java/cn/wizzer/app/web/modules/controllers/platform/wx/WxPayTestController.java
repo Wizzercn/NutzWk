fea051cfc08de7975887dd4629db876dc71264fa
Wizzer <wizzer@qq.com>
2017-03-23 21:34:30 +0800

    add: 微信给个人发送普通红包&裂变红包测试代码

--- wk-app/wk-web/src/main/java/cn/wizzer/app/web/modules/controllers/platform/wx/WxPayTestController.java
+++ wk-app/wk-web/src/main/java/cn/wizzer/app/web/modules/controllers/platform/wx/WxPayTestController.java
@@ -10,6 +10,7 @@ import org.nutz.dao.Cnd;
 import org.nutz.ioc.loader.annotation.Inject;
 import org.nutz.ioc.loader.annotation.IocBean;
 import org.nutz.json.Json;
+import org.nutz.lang.Files;
 import org.nutz.lang.Lang;
 import org.nutz.lang.Strings;
 import org.nutz.lang.random.R;
@@ -20,9 +21,13 @@ import org.nutz.mvc.annotation.At;
 import org.nutz.mvc.annotation.Ok;
 import org.nutz.mvc.annotation.Param;
 import org.nutz.weixin.bean.WxPayUnifiedOrder;
+import org.nutz.weixin.bean.WxRedPack;
+import org.nutz.weixin.bean.WxRedPackGroup;
 import org.nutz.weixin.spi.WxApi2;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
 import java.util.Date;
 
 /**
@@ -62,10 +67,76 @@ public class WxPayTestController {
             order.setNotify_url("http://" + Globals.AppDomain + "/open/pay/wx/back/sendNotify");
             order.setTrade_type("NATIVE");
             NutMap resp = wxApi2.pay_unifiedorder(payinfo.getString("wxpay_key"), order);
-            String return_code = resp.getString("return_code","");//SUCCESS
-            String code_url = resp.getString("code_url","");
+            String return_code = resp.getString("return_code", "");//SUCCESS
+            String code_url = resp.getString("code_url", "");
             log.debug("resp:::" + Json.toJson(resp));
             return Result.success("system.success", code_url);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Result.error("system.error");
+        }
+    }
+
+    @At
+    @Ok("json")
+    public Object redpack(@Param("redpack") int redpack, @Param("openid") String openid, HttpServletRequest req) {
+        try {
+            Wx_config config = wxConfigService.fetch(Cnd.NEW().limit(1, 1));
+            NutMap payinfo = Json.fromJson(NutMap.class, config.getPayInfo());
+            WxApi2 wxApi2 = wxConfigService.getWxApi2(config.getId());
+            WxRedPack redPack = new WxRedPack();
+            redPack.setNonce_str(R.UU32());
+            redPack.setMch_billno(payinfo.getString("wxpay_mchid") + DateUtil.format(new Date(), "yyyyMMddHHmmss"));
+            redPack.setMch_id(payinfo.getString("wxpay_mchid"));
+            redPack.setWxappid(config.getAppid());
+            redPack.setSend_name("大鲨鱼");
+            redPack.setRe_openid(openid);
+            redPack.setTotal_amount(redpack);
+            redPack.setTotal_num(1);
+            redPack.setWishing("新年快乐");
+            redPack.setClient_ip(Lang.getIP(req));
+            redPack.setAct_name("扫码得红包");
+            redPack.setRemark("扫得越多得的越多！");
+            redPack.setScene_id("PRODUCT_1");
+            File file= new File(Globals.AppRoot + "/WEB-INF/cert/wx/" + config.getId() + ".p12");
+            NutMap resp = wxApi2.send_redpack(payinfo.getString("wxpay_key"), redPack,
+                    file,
+                    payinfo.getString("wxpay_mchid"));
+            log.debug("resp:::" + Json.toJson(resp));
+            return Result.success(Json.toJson(resp));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Result.error("system.error");
+        }
+    }
+
+    @At
+    @Ok("json")
+    public Object redpackGroup(@Param("redpack") int redpack, @Param("openid") String openid, HttpServletRequest req) {
+        try {
+            Wx_config config = wxConfigService.fetch(Cnd.NEW().limit(1, 1));
+            NutMap payinfo = Json.fromJson(NutMap.class, config.getPayInfo());
+            WxApi2 wxApi2 = wxConfigService.getWxApi2(config.getId());
+            WxRedPackGroup redPack = new WxRedPackGroup();
+            redPack.setNonce_str(R.UU32());
+            redPack.setMch_billno(payinfo.getString("wxpay_mchid") + DateUtil.format(new Date(), "yyyyMMddHHmmss"));
+            redPack.setMch_id(payinfo.getString("wxpay_mchid"));
+            redPack.setWxappid(config.getAppid());
+            redPack.setSend_name("大鲨鱼的裂变");
+            redPack.setRe_openid(openid);
+            redPack.setTotal_amount(redpack);
+            redPack.setTotal_num(3);
+            redPack.setAmt_type("ALL_RAND");
+            redPack.setWishing("新年快乐");
+            redPack.setAct_name("扫码得红包");
+            redPack.setRemark("扫得越多得的越多！");
+            redPack.setScene_id("PRODUCT_1");
+            File file= new File(Globals.AppRoot + "/WEB-INF/cert/wx/" + config.getId() + ".p12");
+            NutMap resp = wxApi2.send_redpackgroup(payinfo.getString("wxpay_key"), redPack,
+                    file,
+                    payinfo.getString("wxpay_mchid"));
+            log.debug("resp:::" + Json.toJson(resp));
+            return Result.success(Json.toJson(resp));
         } catch (Exception e) {
             e.printStackTrace();
             return Result.error("system.error");
