a96787005310fb246e6e0cffaad6476a2034e5ee
Wizzer <wizzer@qq.com>
2017-04-04 10:14:45 +0800

    add: Api Token增加redis保存key的实现,集群部署时可选用

--- wk-app/wk-web/src/main/java/cn/wizzer/app/sys/modules/services/impl/SysApiServiceImpl.java
+++ wk-app/wk-web/src/main/java/cn/wizzer/app/sys/modules/services/impl/SysApiServiceImpl.java
@@ -8,8 +8,11 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.impl.crypto.MacProvider;
 import org.nutz.dao.Dao;
+import org.nutz.ioc.aop.Aop;
 import org.nutz.ioc.loader.annotation.IocBean;
 import org.nutz.lang.Files;
+
+import static org.nutz.integration.jedis.RedisInterceptor.jedis;
 
 import java.io.*;
 import java.security.Key;
@@ -24,16 +27,27 @@ public class SysApiServiceImpl extends BaseServiceImpl<Sys_api> implements SysAp
         super(dao);
     }
 
-    public static Key key;
+//    @Aop("redis")
+//    private Key getKey(String appId) throws IOException, ClassNotFoundException {
+//        Key key;
+//        byte[] obj = jedis().get(appId.getBytes());
+//        if (obj != null) {
+//            ObjectInputStream keyIn = new ObjectInputStream(new ByteArrayInputStream(obj));
+//            key = (Key) keyIn.readObject();
+//            keyIn.close();
+//        } else {
+//            key = MacProvider.generateKey();
+//            ByteArrayOutputStream bao = new ByteArrayOutputStream();
+//            ObjectOutputStream oos = new ObjectOutputStream(bao);
+//            oos.writeObject(key);
+//            obj = bao.toByteArray();
+//            jedis().set(appId.getBytes(), obj);
+//        }
+//        return key;
+//    }
 
-    /**
-     * 生成token
-     *
-     * @param date 失效时间
-     * @param appId  appId
-     * @return
-     */
-    public String generateToken(Date date, String appId) throws IOException, ClassNotFoundException {
+    private Key getKey(String appId) throws IOException, ClassNotFoundException {
+        Key key;
         File f = new File(Globals.AppRoot + "/WEB-INF/apikey/" + appId + ".key");
         if (Files.isFile(f)) {
             ObjectInputStream keyIn = new ObjectInputStream(new FileInputStream(f));
@@ -46,10 +60,20 @@ public class SysApiServiceImpl extends BaseServiceImpl<Sys_api> implements SysAp
             keyOut.writeObject(key);
             keyOut.close();
         }
+        return key;
+    }
 
+    /**
+     * 生成token
+     *
+     * @param date  失效时间
+     * @param appId appId
+     * @return
+     */
+    public String generateToken(Date date, String appId) throws IOException, ClassNotFoundException {
         return Jwts.builder()
                 .setSubject(appId)
-                .signWith(SignatureAlgorithm.HS512, key)
+                .signWith(SignatureAlgorithm.HS512, getKey(appId))
                 .setExpiration(date)
                 .compact();
     }
@@ -63,12 +87,7 @@ public class SysApiServiceImpl extends BaseServiceImpl<Sys_api> implements SysAp
      */
     public boolean verifyToken(String appId, String token) {
         try {
-            if (key == null) {
-                ObjectInputStream keyIn = new ObjectInputStream(new FileInputStream(Globals.AppRoot + "/WEB-INF/apikey/" + appId + ".key"));
-                key = (Key) keyIn.readObject();
-                keyIn.close();
-            }
-            return Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody().getSubject().equals(appId);
+            return Jwts.parser().setSigningKey(getKey(appId)).parseClaimsJws(token).getBody().getSubject().equals(appId);
         } catch (Exception e) {
             return false;
         }
