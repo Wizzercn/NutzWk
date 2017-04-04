74dd7bc8f989db26f7a3dfbb8877a336d87c00f1
Wizzer <wizzer@qq.com>
2017-04-04 15:47:06 +0800

    ..

--- wk-app/wk-web/src/main/java/cn/wizzer/app/web/modules/controllers/open/api/test/ApiTestController.java
+++ wk-app/wk-web/src/main/java/cn/wizzer/app/web/modules/controllers/open/api/test/ApiTestController.java
@@ -19,40 +19,33 @@ import javax.servlet.http.HttpServletRequest;
 @Filters({@By(type = TokenFilter.class)})
 public class ApiTestController {
     private static final Log log = Logs.get();
-//    @Inject
-//    private ApiTestService apiTestService;
 
     /**
-     * @api {post} /open/api/test/hi 测试API
+     * @api {post} /open/api/test/hi?appid=appid&token=token 测试API
      * @apiGroup Test
      * @apiVersion 1.0.0
      * @apiPermission token
-     * @apiParam {String}	appId 					appId
-     * @apiParam {String}	token 				    token
-     * @apiParam {Object}	data 				    数据对象 
-     * @apiParam {String}	data.txt			    测试文本
+     * @apiParam {Object}	data 				    数据对象
      * @apiParamExample {json} 示例
-     * POST /open/api/test/hi
+     * POST /open/api/test/hi?appid=appid&token=token
      * {
-     *      "appId": "appId",
-     *      "token": "token",
-     *      "data": "\"txt\": \"你好，大鲨鱼\""
+     * "txt": ""你好，大鲨鱼"
      * }
      * @apiSuccess {number} code 			         code
      * @apiSuccess {String} msg 			         msg
      * @apiSuccessExample {json} 示例
      * HTTP/1.1 200 OK
      * {
-     *      "code": 0,
-     *      "msg": "ok"
+     * "code": 0,
+     * "msg": "ok"
      * }
      * @apiError (失败) {number} code 不等于0
      * @apiError (失败) {string} msg 错误文字描述
      * @apiErrorExample {json} 示例
      * HTTP/1.1 200 OK
      * {
-     *      "code": 1,
-     *      "msg": "fail"
+     * "code": 1,
+     * "msg": "fail"
      * }
      */
     @At
@@ -61,8 +54,6 @@ public class ApiTestController {
     public Object hi(@Param("..") NutMap map, HttpServletRequest req) {
         try {
             log.debug("map::" + Json.toJson(map));
-//            Api_test test = Json.fromJson(Api_test.class, Strings.sNull(map.get("data")));
-//            apiTestService.testTx(test);
             return Result.success("ok");
         } catch (Exception e) {
             return Result.error("fail");
