1398e556f83626be24a6b55789e2bcc34204baec
wizzercn <wizzer@qq.com>
2017-09-08 16:54:22 +0800

    del: 删除没必要的文件夹

--- wk-framework/src/main/java/cn/wizzer/framework/ig/IdGenerator.java
+++
@@ -1,7 +0,0 @@
-package cn.wizzer.framework.ig;
-
-import org.nutz.el.opt.RunMethod;
-
-public interface IdGenerator extends RunMethod {
-    String next(String tableName, String prefix) throws Exception;
-}
